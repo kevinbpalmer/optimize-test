ae6b43b81e6eacdeecd838ce0f1d7446dfa61b2b
kevinbpalmer <kevin.bradley.palmer@gmail.com>
2019-07-18 14:11:50 -0500

    init commit

---
+++ server.js
@@ -0,0 +1,10 @@
+// in sublime
+var express = require('express');
+var port = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static('public'));
+
+app.listen(port, function () {
+  console.log(`Example app listening on port !`);
+});
